067a29aa0be122f5e06412a8414a0ac3f55130b7
vyshnavimanthena <144220769+vyshnavimanthena@users.noreply.github.com>
2023-10-23 22:41:48 +0530

    Add files via upload

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>me</h1>
+      <p>Web Developer</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      {/* Add your project components or details here */}
+    </section>
+  );
+}
+
+export default Projects;
+
